ffe63a2f300be5942f1d8a7c410c69139b07f3c5
ruicong <kwanruicong6462@gmail.com>
2025-03-29 22:38:44 +0800

    added layout, activity view

--- app/src/main/java/com/example/cs205_ass4/MainActivity.kt
+++ app/src/main/java/com/example/cs205_ass4/MainActivity.kt
@@ -10,20 +10,38 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.cs205_ass4.game.GameScreen
 import com.example.cs205_ass4.ui.theme.Cs205ass4Theme
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : ComponentActivity() {
+// class MainActivity : ComponentActivity() {
+//     override fun onCreate(savedInstanceState: Bundle?) {
+//         super.onCreate(savedInstanceState)
+//         enableEdgeToEdge()
+//         setContent {
+//             Cs205ass4Theme {
+//                 Surface(
+//                     modifier = Modifier.fillMaxSize(),
+//                     color = MaterialTheme.colorScheme.background
+//                 ) {
+//                     GameScreen()
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Cs205ass4Theme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GameScreen()
-                }
-            }
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
