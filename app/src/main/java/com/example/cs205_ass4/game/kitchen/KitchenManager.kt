e52d44523c4ff546cb74e0035b6cae304dfd7abf
ruicong <kwanruicong6462@gmail.com>
2025-04-15 22:12:32 +0800

    removed redundant kitchen methods, get ktichen, take order, get expired order ids

--- app/src/main/java/com/example/cs205_ass4/game/kitchen/KitchenManager.kt
+++ app/src/main/java/com/example/cs205_ass4/game/kitchen/KitchenManager.kt
@@ -4,24 +4,12 @@ class KitchenManager {
     private val kitchen = Kitchen()
     private var onOrdersExpiredCallback: ((List<Int>) -> Unit)? = null
 
-    fun getKitchen(): Kitchen {
-        return kitchen
-    }
-
     fun addOrder(order: Order): Boolean {
         return kitchen.kitchenCounter.addOrder(order)
     }
 
-    fun takeOrder(): Order? {
-        return kitchen.kitchenCounter.takeOrder()
-    }
-
     fun getOrders(): List<Order> {
         return kitchen.kitchenCounter.getOrders()
-    }
-
-    fun getExpiredOrderIds(): List<Int> {
-        return kitchen.kitchenCounter.getExpiredOrderIds()
     }
 
     fun setOnOrdersExpiredCallback(callback: (List<Int>) -> Unit) {
